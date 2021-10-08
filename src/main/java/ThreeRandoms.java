5bcc51a2b5616b25349e14b56a0fc1bff5eb442a
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-10-08 00:49:06 -0700

    Update ThreeRandoms.java

--- src/main/java/ThreeRandoms.java
+++ src/main/java/ThreeRandoms.java
@@ -1,13 +1,7 @@
 public class ThreeRandoms {
 
-	//static helloYou
-	// take a string and print out "Hello STR"
-
-	//static countDog
-	// count how many times the word dog appears in a given string
-
-	//flipFlop 
-	// return true if given two ints one is 
-	//positive and one is negative
+	public static String helloYou(String word) {
+		return "Hello Blank";
+	}
 
 }
